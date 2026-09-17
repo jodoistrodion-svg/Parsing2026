56e8b000e83f13f188846fe8711cd2f3ac78999f
jodoistrodion-svg <jodoist.rodion@gmail.com>
2026-09-17 12:23:09 +0700

    refactor v2: restore domain package boundary

---
+++ domain/__init__.py
@@ -0,0 +1,4 @@
+from .models import DiscoveryResult, MarketItem, MarketSource, PurchaseResult
+from .decision import Decision, DecisionEngine
+
+__all__ = ["Decision", "DecisionEngine", "DiscoveryResult", "MarketItem", "MarketSource", "PurchaseResult"]
