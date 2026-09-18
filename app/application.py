f6598c8eabd5fc2ce1b137470e1075bb973b7973
jodoistrodion-svg <jodoist.rodion@gmail.com>
2026-09-18 22:08:40 +0700

    make composition-root side-effect imports explicit

--- app/application.py
+++ app/application.py
@@ -5,6 +5,9 @@ from app.runtime.core import autobuy_queue_manager, dp, has_valid_telegram_token
 from app.purchase import autobuy as _autobuy
 from app import handlers as _handlers
 
+# These imports register aiogram handlers and autobuy helpers at module import time.
+_BOOTSTRAPPED_MODULES = (_autobuy, _handlers)
+
 __all__ = [
     "API_TOKEN", "LZT_BALANCE_ID", "autobuy_queue_manager", "dp",
     "has_valid_telegram_token", "logger", "_safe_compact",
