9352e8448fb2f490cd07b188fa1070b8d4503a19
jodoistrodion-svg <jodoist.rodion@gmail.com>
2026-09-18 14:28:14 +0700

    style: remove duplicate SQLite import

--- app/storage/sqlite.py
+++ app/storage/sqlite.py
@@ -8,8 +8,6 @@ import aiosqlite
 from app.config.settings import ACCESS_OPEN, DB_FILE, OWNER_IDS, SEED_URLS_JSON
 from market.normalize import normalize_url, validate_market_url
 
-import aiosqlite
-
 _db: aiosqlite.Connection | None = None
 _db_lock = asyncio.Lock()
 
